85ca528c29a60eb9870ffd1f549c3779f5973357
Anton Rogov <anton.s.rogov@gmail.com>
2010-05-03 15:03:39 +0400

    adding ajax

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,41 @@ module ApplicationHelper
       h(user.name)
     end
   end
+  
+  def custom_will_paginate(collection, options = {})
+    html = '<ul id="pagination">'
+    
+    if collection.current_page > 1
+      content = custom_will_paginate_link("&laquo; Previous", collection, collection.current_page - 1, options)
+    else
+      content = "&laquo; Previous"
+    end
+    html << '<li class="prev">' + content + '</li>'
+    
+    collection.total_pages.times do |i|
+      page = i + 1
+      if page == collection.current_page
+        html << '<li class="active">' + page.to_s
+      else
+        html << '<li>' + custom_will_paginate_link(page, collection, page, options)
+      end
+      html << '</li>'
+    end
+    
+    if collection.current_page < collection.total_pages
+      content = custom_will_paginate_link("Next &raquo;", collection, collection.current_page + 1, options)
+    else
+      content = "Next &raquo;"
+    end
+    html << '<li class="next">' + content + '</li>'
+    
+    html << '</ul>'
+    html
+  end
+  
+  def custom_will_paginate_link(content, collection, page, options = {})
+    param_name = options[:param] || :page
+    url_params = page > 1 ? { param_name => page } : {}
+    link_to(content, url_for(url_params))
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,22 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var ajax = null;
+
+function matches_selector(obj, selector) {
+  if(selector[0] == '.') {
+    return obj.hasClassName(selector.substring(1));
+  } else if(selector[0] == '#') {
+    return obj.getId() == selector.substring(1);
+  } else {
+    return obj.getTagName() == selector.toUpperCase();
+  }
+}
+
+function foreach(obj, selector, callback) {
+  var children = obj.getChildNodes();
+  for(var i = 0; i < children.length; i++) {
+    var child = children[i];
+    if(matches_selector(child, selector)) {
+      callback(child);
+    }
+    foreach(child, selector, callback);
+  }
+}
